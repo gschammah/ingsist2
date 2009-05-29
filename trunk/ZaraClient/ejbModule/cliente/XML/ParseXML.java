a25a997ecaa0b378844a47916ac06f49591b83e9
gschammah <gschammah@52ee7990-2db6-11de-acd2-81e04a9d018a>
2009-05-29 07:18:26 +0000

    Ya Parsea el ofad.xml y lo manda al server

--- trunk/ZaraClient/ejbModule/cliente/XML/ParseXML.java
+++ trunk/ZaraClient/ejbModule/cliente/XML/ParseXML.java
@@ -1,41 +1,114 @@
 package cliente.XML;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
 import com.thoughtworks.xstream.XStream;
 
+import server.VO.OfAD.ItemOfADVO;
+import server.VO.OfAD.OfADVO;
+import server.VO.articulos.ArtHogarVO;
+import server.VO.articulos.ArtRopaVO;
+import server.VO.articulos.ArticuloVO;
+import server.entidades.OfAD.ItemOfAD;
 import server.entidades.OfAD.OfAD;
-import cliente.tools.FileReaderWrapper;
+import cliente.tools.MD5;
 
 public class ParseXML {
-	
-	private String file; 
 
-	public static void main(String[] args) {
-		new ParseXML("test.xml");
-	}
-	
-	public ParseXML(String file) {
-		this.file = file;
-	}
-	
-	public OfAD parseOfAD(){
+	public static OfADVO parseOfAD(String file) {
+
+		SAXBuilder builder = new SAXBuilder();
+		Document doc = null;
 		
-		FileReaderWrapper fileReader = new FileReaderWrapper(this.file);		
-		String contenidoXML = fileReader.obtenerContenido();
+		MD5 md5 = new MD5(file);
+								
+		try {			
+			doc = builder.build(new FileInputStream(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Element root = doc.getRootElement();
+		List articulos = root.getChild("articulos").getChildren();
+		OfADVO ofadVO = new OfADVO();
+		ofadVO.setFecha(Calendar.getInstance().getTime());				
+		ofadVO.setId(md5.obtenerMD5Hash());		
+		
+		//TODO hacer que sea autonumerico
+		int i = 1;
+				
+		for (Object articulo : articulos) {
 
-			XStream xstream = new XStream();
-			xstream.alias("OfadVo", OfAD.class);
-			OfAD OfADXML = (OfAD)xstream.fromXML(contenidoXML);
+			Element art = ((Element) articulo);
+
+			ItemOfADVO itemOfadVO = new ItemOfADVO();
+			itemOfadVO.setPrecioLista(Double.parseDouble(art
+					.getChildText("precioLista")));
+			itemOfadVO.setPrecioOferta(Double.parseDouble(art
+					.getChildText("precioOferta")));
+			itemOfadVO.setId(i++);
+
+			if (art.getName() == "xml.ArtRopaVO") {
+
+				ArtRopaVO nuevoArt = new ArtRopaVO();
+
+				nuevoArt.setColor(art.getChildText("color"));
+				nuevoArt.setDescripcion(art.getChildText("descripcion"));
+				nuevoArt.setLinea(art.getChildText("linea"));
+				nuevoArt.setPrecioLista(Double.parseDouble(art
+						.getChildText("precioLista")));
+				nuevoArt.setPrecioOferta(Double.parseDouble(art
+						.getChildText("precioOferta")));
+				nuevoArt.setReferencia(Long.parseLong(art
+						.getChildText("Referencia")));
+				nuevoArt.setSeccion(art.getChildText("seccion"));
+				nuevoArt.setOrigen(art.getChildText("origen"));
+				nuevoArt.setTalle(art.getChildText("talle"));
+
+				itemOfadVO.setArticulo(nuevoArt);
+
+			} else if (art.getName() == "xml.ArtHogarVO") {
+
+				ArtHogarVO nuevoArt = new ArtHogarVO();
+
+				nuevoArt.setColor(art.getChildText("color"));
+				nuevoArt.setDescripcion(art.getChildText("descripcion"));
+				nuevoArt.setLinea(art.getChildText("linea"));
+				nuevoArt.setPrecioLista(Double.parseDouble(art
+						.getChildText("precioLista")));
+				nuevoArt.setPrecioOferta(Double.parseDouble(art
+						.getChildText("precioOferta")));
+				nuevoArt.setReferencia(Long.parseLong(art
+						.getChildText("Referencia")));
+				nuevoArt.setSeccion(art.getChildText("seccion"));
+				nuevoArt.setCategoria(art.getChildText("categoria"));
+				nuevoArt.setComposicion(art.getChildText("composicion"));
+				nuevoArt.setMedidas(art.getChildText("medidas"));
+				nuevoArt.setNombre(art.getChildText("nombre"));
+
+				itemOfadVO.setArticulo(nuevoArt);				
+			}
 			
-			return OfADXML;
-	
-			/*for (int i=0; i<l.getArticulos().size(); i++) {
-				if (l.getArticulos().get(i) instanceof ArtRopaVO) {
-					System.out.println("IDENTIFICADO COMO ROPA: " + l.getArticulos().get(i).getDescripcion());
-				}
-				if (l.getArticulos().get(i) instanceof ArtHogarVO) {
-					System.out.println("IDENTIFICADO COMO HOGAR: " + l.getArticulos().get(i).getDescripcion());
-				}
-			}*/			
+			ofadVO.addItem(itemOfadVO);
+
+		}
+
+		return ofadVO;
+
 	}
-	
 }
--- trunk/ZaraClient/ejbModule/cliente/test.java
+++ trunk/ZaraClient/ejbModule/cliente/test.java
@@ -1,54 +1,45 @@
 package cliente;
 
 import java.util.Hashtable;
-
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import cliente.vistas.MainMenu;
-
-import com.thoughtworks.xstream.XStream;
-
-import server.VO.articulos.ArtHogarVO;
-import server.VO.articulos.ArticuloVO;
-import server.beans.articulos.*;
-
-
+import cliente.XML.ParseXML;
+import server.VO.OfAD.OfADVO;
+import server.beans.articulos.AdministradorArticulos;
 
 public class test {
 
-	 private Hashtable<String, String> contextProperties;
-     private InitialContext initialContext;
+	private Hashtable<String, String> contextProperties;
+	private InitialContext initialContext;
 
 	public static void main(String[] args) {
-				
-		 new test();
-	}
-	
-	public test(){
-		/*AdministradorArticulos admArt;
-		contextProperties = new Hashtable<String, String>();
-        contextProperties.put(InitialContext.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
-        contextProperties.put(InitialContext.PROVIDER_URL, "jnp://127.0.0.1:1099");
-        String naming = "ZaraEAR/AdministradorArticulosBean/remote";
-        try {
-                initialContext = new InitialContext(contextProperties);
-                admArt = (AdministradorArticulos) initialContext.lookup(naming);
-                
-                
-                XStream xstream = new XStream();
-                xstream.alias("ArtHogarVO", ArticuloVO.class);
-                
-            	//ArtHogarVO saco = (ArtHogarVO)xstream.fromXML();
-                                                
-                //admArt.test(art);
 
-        } catch (NamingException e) {
-                e.printStackTrace();
-        }*/
+		new test();
+	}
+
+	public test() {
+
+		AdministradorArticulos admArt;
+		contextProperties = new Hashtable<String, String>();
+		contextProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+		contextProperties.put(Context.PROVIDER_URL, "jnp://127.0.0.1:1099");
+		String naming = "ZaraEAR/AdministradorArticulosBean/remote";
 		
-		MainMenu main = new MainMenu();
-		main.setVisible(true);
+		try {
+			initialContext = new InitialContext(contextProperties);
+			admArt = (AdministradorArticulos) initialContext.lookup(naming);
+			OfADVO ofadVO = ParseXML.parseOfAD("/UADE/workspace/ZaraClient/ejbModule/cliente/XML/OFAD.xml");
+			admArt.test(ofadVO);
+
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+		// MainMenu main = new MainMenu();
+		// main.setVisible(true);
+
 	}
 
 }

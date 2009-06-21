097f83aac58c377c70c558dd4c9d80bd01131b3e
gschammah <gschammah@52ee7990-2db6-11de-acd2-81e04a9d018a>
2009-06-21 21:51:34 +0000

    Esto es lo que entregamos el 22-6-09

---
+++ tags/Servidor_Entregado-22-06-09/ejbModule/server/VO/articulos/ArticuloVO.java
@@ -0,0 +1,139 @@
+/*
+ * Ingeniería en sistemas 2
+ * TP Tienda Zara - Grupo 13
+ * 
+ * Integrantes:
+ * Gabriel Schammah
+ * Maximiliano Landivar
+ */
+package server.VO.articulos;
+
+import java.io.Serializable;
+
+
+public class ArticuloVO implements Serializable {
+		
+	private static final long serialVersionUID = 1L;
+	
+	private long referencia;
+	private String linea;
+	private String descripcion;
+	private float precioLista;
+	private float precioOferta;
+	private String color;
+	private String seccion;
+	private int stock;
+	private boolean nuevo = false;
+	private int puntoReposicion;
+	
+	public ArticuloVO(long referencia, String linea, String descripcion, float precioLista, float precioOferta, String color, String seccion, int stock) {
+		this.referencia = referencia;
+		this.linea = linea;
+		this.descripcion = descripcion;
+		this.precioLista = precioLista;
+		this.precioOferta = precioOferta;
+		this.color = color;
+		this.seccion = seccion;
+		this.stock = stock;
+	}
+
+	public ArticuloVO() {
+	}
+
+	public long getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(long referencia) {
+		this.referencia = referencia;
+	}
+
+	public String getLinea() {
+		return linea;
+	}
+
+	public void setLinea(String linea) {
+		this.linea = linea;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public float getPrecioLista() {
+		return precioLista;
+	}
+
+	public void setPrecioLista(float precioLista) {
+		this.precioLista = precioLista;
+	}
+
+	public float getPrecioOferta() {
+		return precioOferta;
+	}
+
+	public void setPrecioOferta(float precioOferta) {
+		this.precioOferta = precioOferta;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getSeccion() {
+		return seccion;
+	}
+
+	public void setSeccion(String seccion) {
+		this.seccion = seccion;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public boolean isNuevo() {
+		return nuevo;
+	}
+
+	public void setNuevo(boolean nuevo) {
+		this.nuevo = nuevo;
+	}
+
+	public int getPuntoReposicion() {
+		return puntoReposicion;
+	}
+
+	public void setPuntoReposicion(int puntoReposicion) {
+		this.puntoReposicion = puntoReposicion;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		
+		result.append("<b>Descripción:</b> " + descripcion + "<br>");
+		result.append("<b>Línea:</b> " + linea + "<br>");
+		result.append("<b>Color:</b> " + color + "<br>");
+		
+		return result.toString();
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		return true;
+	}
+ 			
+}
---
+++ tags/Servidor_Entregado-22-06-09/ejbModule/server/beans/ventas/AdministradorVentasBean.java
@@ -0,0 +1,105 @@
+/*
+ * Ingeniería en sistemas 2
+ * TP Tienda Zara - Grupo 13
+ * 
+ * Integrantes:
+ * Gabriel Schammah
+ * Maximiliano Landivar
+ */
+package server.beans.ventas;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import server.VO.ventas.VentaVO;
+import server.beans.articulos.AdministradorArticulos;
+import server.entidades.articulos.Articulo;
+import server.entidades.ventas.ItemVenta;
+import server.entidades.ventas.Venta;
+
+// TODO: Auto-generated Javadoc
+/**
+ * Bean que Administrada las Ventas.
+ */
+@Stateful
+public class AdministradorVentasBean implements AdministradorVentas {
+
+	/** EntityManager */
+	@PersistenceContext
+	private EntityManager em;
+
+	/** Bean Administrador Articulos. */
+	@EJB(name = "ZaraEAR/AdministradorArticulosBean/local")
+	AdministradorArticulos admArticulos;
+	
+	/**
+	 * Recibe la lista de artículos facturados y calcula la cantidad
+	 * total pedida del mismo artículo
+	 * 
+	 * @param itemVenta Línea de la factura
+	 * @param items Todas las líneas de la factura.
+	 * 
+	 * @return Cantidad total 
+	 */
+	private int mergeArticulos(ItemVenta itemVenta, Collection<ItemVenta> items) {
+		
+		int result = 0;
+		
+		for (int i = 0; i < items.size(); i++) {
+			ItemVenta item = ((ArrayList<ItemVenta>) items).get(i);
+			if (item.getArticulo().getReferencia() == itemVenta.getArticulo().getReferencia()) {
+				result += item.getCantidad();
+			}
+		}
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see server.beans.ventas.AdministradorVentas#nuevaVenta(server.VO.ventas.VentaVO)
+	 */
+	public VentaVO nuevaVenta(VentaVO vo) {
+		
+		Collection<ItemVenta> result = new ArrayList<ItemVenta>();		
+		
+		Venta venta = new Venta();
+		venta.setVO(vo);
+		
+		Collection<ItemVenta> items = venta.getItems();
+
+		for (ItemVenta item : items) {									
+
+			Articulo art = admArticulos.buscarArticulo(item.getArticulo().getReferencia());
+			
+			if (art.getStock() < mergeArticulos(item, items)) {
+				ItemVenta i = new ItemVenta();
+				i.setArticulo(art);
+				result.add(i);
+			}
+			else {
+				art.setStock(art.getStock() - item.getCantidad());
+				item.setArticulo(art);
+			}
+		}
+		
+
+		if (result.size() != 0) {
+			// no hay stock
+			venta.setHayStock(false);
+			venta.setItems(result);
+		} else {
+			// hay stock
+			for (ItemVenta item : items) {
+				em.merge(item.getArticulo());
+			}
+			em.persist(venta);
+			venta.setHayStock(true);			
+		}
+		return venta.getVO();
+	}
+
+}

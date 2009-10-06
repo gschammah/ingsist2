df1ff809e650fb475b9e6e85061ab00921b0f42c
gschammah <gschammah@52ee7990-2db6-11de-acd2-81e04a9d018a>
2009-10-06 04:44:44 +0000

    saque el boton registrar

--- ZaraWeb/src/ar/edu/uade/ingsoft/pages/ConfirmaOfad.java
+++ ZaraWeb/src/ar/edu/uade/ingsoft/pages/ConfirmaOfad.java
@@ -3,8 +3,11 @@ package ar.edu.uade.ingsoft.pages;
 import javax.naming.NamingException;
 
 import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.PageAttached;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
+import org.apache.tapestry5.services.Session;
 
 import server.VO.OfAD.ItemOfADVO;
 import server.VO.OfAD.OfADVO;
@@ -18,20 +21,25 @@ public class ConfirmaOfad extends MainPage {
 	
 	@Property
 	private ItemOfADVO item;
+	
+	@Property
+	@Persist
+	private boolean success;
 			
 	private Class action;
 	
 	public Object onSuccess() {				
 		return action;				
 	}
-	
+		
 	private void onSelectedFromGuardar() {
 		super.init();
 		ofad = getFachada().nuevoOfad(ofad, true);
+		success = true;
 		action = null;
 	}
 	
-	private void onSelectedFromSalir() {
+	private void onSelectedFromSalir() {		
 		action = Index.class;
 	} 
 	
